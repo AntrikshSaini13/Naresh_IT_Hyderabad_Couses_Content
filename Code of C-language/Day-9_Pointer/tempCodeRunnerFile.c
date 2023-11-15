829f8e1a04915ee7cd31b6597d084f34cb384931
AntrikshSaini13 <antrikshsaini13@gmail.com>
2023-11-16 01:04:42 +0530

    pointer

--- Code of C-language/Day-9_Pointer/tempCodeRunnerFile.c
+++ Code of C-language/Day-9_Pointer/tempCodeRunnerFile.c
@@ -1,3 +1,15 @@
-int n=10;
-    // float *p;
-    // p=&n;
+//Creating dynamic one-dimensional array: with help of malloc()
+
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+    int n, i, *p;
+    printf("Enter array size = ");scanf("%d",&n);
+    p=(int*)malloc(n*sizeof(int));
+    printf("Enter %d integer number = ",n);
+    for(i=0;i<n;i++) scanf("%d",(p+i));
+    puts("Array Elements ");
+    for(i=0;i<n;i++)printf("%4d",*(p+i));
+    free(p);
+    p=NULL;
+}
