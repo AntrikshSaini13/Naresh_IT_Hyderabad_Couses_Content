9e57f14bce9c4bd1fc43a7f2aceb5b276e1cd4fc
AntrikshSaini13 <antrikshsaini13@gmail.com>
2023-11-29 22:44:37 +0530

    function

--- Code of C-language/Day-10_Function/Question-6.c
+++ Code of C-language/Day-10_Function/Question-6.c
@@ -1,38 +1,28 @@
+// Write a program in C to add n natural numbers using a function.
+
+// Test Data :
+
+// Input n value : 10
+
+// Expected Output :
+
+// The sum of 10 natural numbers is 55
+
 #include <stdio.h>
-#include <math.h>
-int isArmstrong(int num);
 
-void power(int base, int exponent);
-
-void power(int base, int exponent) {
-   int k,i,j,count,sum,num,power,d,p;
-
-   printf("%d %d\n",base, exponent);
-
-   for(i=base;i<=exponent;i++){
-        //power count
-        num=i;
-        power=0;
-        while(num){
-            num/=10;
-            power++;
-        }
-        //printf("%d\n",power);
-        for(j=i;j!=0;){
-            d=j%10;
-            j/=10;            sum+=p;
-            }
-        }
-        if(sum==i) 
-            printf("%d\n",i);
-        //printf("%d\n",i);
-    //return 0;
-    }
+// Function to calculate the sum of n natural numbers
+int sum(int num) {
+    int sum=0,i;
+    //Write Your Code Here...
+    for(i=1;i<=num;i++)
+        sum+=i;
+    return sum;
 }
-int main() {
-        //write your code here...r");
-    scanf("%d",&num1);
-    puts("Enter the Ending of armsrong number");    scanf("%d",&num2);
-    power(num1,num2);
+int main(){
+    //Write Your Code Here...
+    int num;
+    puts("Ente any number");
+    scanf("%d",&num);
+    printf("Sum of Natural Number is %d",sum(num));
     return 0;
 }
