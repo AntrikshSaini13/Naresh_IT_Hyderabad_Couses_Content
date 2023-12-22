d28de42e6da17c49466c3927ae4febd07204cd3c
AntrikshSaini13 <antrikshsaini13@gmail.com>
2023-12-23 03:04:15 +0530

    java

---
+++ Java_By_Ravi_Shanker/Java_Class_Code/IntegralLeterals/OctalLeteral.java
@@ -0,0 +1,9 @@
+package Java_By_Ravi_Shanker.Java_Class_Code.IntegralLeterals;
+
+public class OctalLeteral {
+    {
+		short s = 127;
+		byte b = (byte) s;  //Explicit type casting [Chance of data loss]
+		System.out.println(b);
+	}
+}
--- Java_By_Ravi_Shanker/Java_Lab_Tasks/Day_1_Task/Welcome.java
+++ Java_By_Ravi_Shanker/Java_Lab_Tasks/Day_1_Task/Welcome.java
@@ -2,6 +2,6 @@ package Java_By_Ravi_Shanker.Java_Lab_Tasks.Day_1_Task;
 
 public class Welcome {
     public static void main(String[] args) {
-        System.out.println("Welcome");
+        System.out.println("Hello");
     }
 }
