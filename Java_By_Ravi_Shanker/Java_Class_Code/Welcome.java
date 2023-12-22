2ab52432c897a89c0310c80ee93e400134416e3a
AntrikshSaini13 <antrikshsaini13@gmail.com>
2023-12-22 22:41:46 +0530

    delete

---
+++ Java_By_Ravi_Shanker/Java_Class_Code/Welcome.java
@@ -0,0 +1,7 @@
+package Java_By_Ravi_Shanker.Java_Class_Code;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Java");
+    }
+}
--- Java_By_Ravi_Shanker/Java_Lab_Tasks/Day_1_Task/Welcome.java
+++ Java_By_Ravi_Shanker/Java_Lab_Tasks/Day_1_Task/Welcome.java
@@ -1,4 +1,4 @@
-package Java_By_Ravi_Shanker.Lab_Tasks.Day_1_Task;
+package Java_By_Ravi_Shanker.Java_Lab_Tasks.Day_1_Task;
 
 public class Welcome {
     public static void main(String[] args) {
