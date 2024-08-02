166662f44e933bc93a4ed194d33e54de03a03d7d
AntrikshSaini13 <antrikshsaini13@gmail.com>
2024-08-02 23:24:05 +0530

    update

---
+++ 3_Core_Java_By_Ravi_Shanker_Sir/2.Java_Lab_Tasks/Core_Java_Codes/src/Mar_09_2024_Lab_Task/MCQQuestions.java
@@ -0,0 +1,5 @@
+package Mar_09_2024_Lab_Task;
+
+public class MCQQuestions {
+
+}
---
+++ 3_Core_Java_By_Ravi_Shanker_Sir/2.Java_Lab_Tasks/Core_Java_Codes/src/Mar_09_2024_Lab_Task/Question_1.java
@@ -0,0 +1,7 @@
+package Mar_09_2024_Lab_Task;
+
+public class Question_1 {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
